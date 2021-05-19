02777170b58842664165f05a93029483a908a1ba
whitefox <deadfox8900@gmail.com>
2021-05-19 17:41:30 +0300

    add API test 'work with folder'

--- helpers/api.ts
+++ helpers/api.ts
@@ -1,22 +1,55 @@
-import { elements } from "../pages/main";
-module.exports = {
-  customHeaders: {
-    Autorization: "OAuth AQAAAAAcXMInAAcf7RtGGcq-m0wJn_H1SFrpFDY",
-    Accept: "*/*"
-  },
-  URL: "https://cloud-api.yandex.net/v1/disk/",
+import fetch from "node-fetch";
+const expect = require("expect");
 
-},
+// interface ICreateFolder {
+//   href: string;
+//   method: string;
+//   templated: boolean;
+// }
 
-async createFolder(foldterNAme: string) {
-
-}
+// interface IError {
+//   message: string;
+//   description: string;
+//   error: string;
+// }
 
 class API {
-  async print(name: string): Promise<string> {
-    return name + "a";
+  private customHeaders: any = {
+    Authorization: "OAuth AQAAAAAcXMInAAcf7RtGGcq-m0wJn_H1SFrpFDY",
+    Accept: "*/*",
+  };
+  private URL: string = "https://cloud-api.yandex.net/v1/disk/";
+
+  async createFolder(folderName: string): Promise<any> {
+    const response = await fetch(`${this.URL}resources?path=${folderName}`, {
+      headers: this.customHeaders,
+      method: "PUT",
+    });
+    return await response.json();
+  }
+
+  async isCreate(response: any, folderName: string): Promise<void> {
+    expect(response.path).toEqual(`disk:/${folderName}`);
+  }
+
+  async isDelete(response: any, folderName: string): Promise<void> {
+    expect(response.message).toEqual("Не удалось найти запрошенный ресурс.");
+  }
+
+  async deleteFolder(folderName: string): Promise<void> {
+    const response = await fetch(`${this.URL}resources?path=${folderName}`, {
+      headers: this.customHeaders,
+      method: "DELETE",
+    });
+  }
+
+  async getFolder(folderName: string): Promise<void> {
+    const response = await fetch(`${this.URL}resources?path=${folderName}`, {
+      headers: this.customHeaders,
+      method: "GET",
+    });
+    return await response.json();
   }
 }
-
 
 export default new API();
--- tests API/API1_test.ts
+++ tests API/API1_test.ts
@@ -3,5 +3,10 @@ import api from "../helpers/api";
 Feature("Работа с папкой");
 
 Scenario("Создать/удалить папку", async () => {
-  api.print("Dasha");
+  await api.createFolder("v");
+  const create = await api.getFolder("v");
+  await api.isCreate(create, "v");
+  await api.deleteFolder("v");
+  const deleted = await api.getFolder("v");
+  await api.isDelete(deleted, "v");
 });
